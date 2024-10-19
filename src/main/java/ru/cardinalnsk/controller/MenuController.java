b0b25a24db3621a29377c706b312a7bb7014661b
Cardinalnsk <andron.nsk.54@gmail.com>
2024-10-20 00:56:51 +0700

    add initialization MenuController class

--- src/main/java/ru/cardinalnsk/controller/MenuController.java
+++ src/main/java/ru/cardinalnsk/controller/MenuController.java
@@ -1,5 +1,28 @@
 package ru.cardinalnsk.controller;
 
+import java.util.List;
+import ru.cardinalnsk.model.Menu;
+import ru.cardinalnsk.view.menu.MenuView;
+import ru.cardinalnsk.view.menu.impl.ConsoleMenuView;
+import ru.cardinalnsk.view.utils.Printer;
+
 public class MenuController {
+
+    private static final Menu STARTUP_MENU = Menu.builder()
+        .header("Welcome to snake game")
+        .menuItems(List.of(
+            "Start game",
+            "Score table",
+            "Exit"
+        ))
+        .build();
+    private final Printer printer;
+    private final MenuView mainMenu;
+
+    public MenuController(Printer printer) {
+        this.printer = printer;
+        this.mainMenu = new ConsoleMenuView(printer, STARTUP_MENU);
+    }
+
 
 }
