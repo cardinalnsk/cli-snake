2574a2c9d280a5cdd557503a6c6c12bd64abb4bf
Cardinalnsk <andron.nsk.54@gmail.com>
2024-10-20 04:37:36 +0700

    implements MenuController

--- src/main/java/ru/cardinalnsk/controller/MenuController.java
+++ src/main/java/ru/cardinalnsk/controller/MenuController.java
@@ -1,28 +1,49 @@
 package ru.cardinalnsk.controller;
 
-import java.util.List;
+import lombok.Getter;
 import ru.cardinalnsk.model.Menu;
 import ru.cardinalnsk.view.menu.MenuView;
 import ru.cardinalnsk.view.menu.impl.ConsoleMenuView;
 import ru.cardinalnsk.view.utils.Printer;
 
-public class MenuController {
+public class MenuController implements Runnable {
 
-    private static final Menu STARTUP_MENU = Menu.builder()
-        .header("Welcome to snake game")
-        .menuItems(List.of(
-            "Start game",
-            "Score table",
-            "Exit"
-        ))
-        .build();
-    private final Printer printer;
-    private final MenuView mainMenu;
+    private final Menu menu;
+    private final MenuView menuView;
+    private final ConsoleController consoleController;
+    boolean isAlive = false;
+    @Getter
+    private int selectedMenuItem = -1;
 
-    public MenuController(Printer printer) {
-        this.printer = printer;
-        this.mainMenu = new ConsoleMenuView(printer, STARTUP_MENU);
+    public MenuController(Printer printer, ConsoleController consoleController, Menu menu) {
+        this.menu = menu;
+        this.menuView = new ConsoleMenuView(printer, menu);
+        this.consoleController = consoleController;
     }
 
+    @Override
+    public void run() {
+        isAlive = true;
+        consoleController.maxMenuItems(menu.menuItems().size());
+        menuView.draw();
+        while (!consoleController.enterPressed()) {
+            menuView.drawSelectedMenuItem(consoleController.selectedMenuItem());
+
+            if (consoleController.enterPressed()) {
+                isAlive = false;
+                selectedMenuItem = consoleController.selectedMenuItem();
+            }
+        }
+        consoleController.enterPressed(false);
+    }
+
+    public void stop() {
+        isAlive = false;
+        try {
+            Thread.currentThread().interrupt();
+        } catch (Exception ignored) {
+
+        }
+    }
 
 }
